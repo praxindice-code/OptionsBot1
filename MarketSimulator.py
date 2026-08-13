cdd3c36c834361d85433c8ada71a3350b10b96a2
praxindice-code <pranayabista6@gmail.com>
2026-08-13 13:35:23 -0700

    blackscholes option pricer

--- MarketSimulator.py
+++ MarketSimulator.py
@@ -1,20 +1,32 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import vollib.black_scholes as bs
 
 n_ticks = 1000 
-Volatility = .02 
+vol = .02
 
-percentChanges = np.random.normal(loc=0, scale=Volatility, size=n_ticks) 
+percentChanges = np.random.normal(loc=0, scale=.02, size=n_ticks) 
 
 ticksDesired = 1000
 s0 = 100
 priceArray = s0 * np.exp(np.cumsum(percentChanges[:ticksDesired]))
 #exp adds one 2 each array, cumsum combines each next element with previous element sum
 
+strike = 150 
+time_to_expire = .1
+rate = .02
+Volatility = .2
+optionPriceList = []
 
+for tick in range(ticksDesired):
+    time_to_expire = time_to_expire - (tick / ticksDesired) * time_to_expire
+    
+    option_price = bs.black_scholes('c', priceArray[tick], strike, time_to_expire, rate, Volatility)
+    optionPriceList.append(option_price) 
+    
 
-plt.plot(priceArray)
-plt.xlabel("time")
-plt.ylabel("price")
+plt.plot(priceArray, label = "Stock Price")
+plt.plot(optionPriceList, label = "Option Price")
+plt.title("Option Price vs Stock Price")
+plt.legend()
 plt.show()
-
