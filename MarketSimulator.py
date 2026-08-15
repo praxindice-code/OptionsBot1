74e1ea35484f358c89c9afd32cdebe1d13d41b52
praxindice-code <pranayabista6@gmail.com>
2026-08-14 21:11:47 -0700

    added a-s equations

--- MarketSimulator.py
+++ MarketSimulator.py
@@ -10,7 +10,7 @@ percentChanges = np.random.normal(loc=0, scale=.02, size=n_ticks)
 ticksDesired = 1000
 s0 = 100
 priceArray = s0 * np.exp(np.cumsum(percentChanges[:ticksDesired]))
-#exp adds one 2 each array, cumsum combines each next element with previous element sum
+# cumsum combines each next element with previous element sum
 
 strike = 150 
 time_to_expire = .1
@@ -22,11 +22,14 @@ for tick in range(ticksDesired):
     time_to_expire = time_to_expire - (tick / ticksDesired) * time_to_expire
     
     option_price = bs.black_scholes('c', priceArray[tick], strike, time_to_expire, rate, Volatility)
-    optionPriceList.append(option_price) 
-    
+    optionPriceList.append(option_price)   
 
-plt.plot(priceArray, label = "Stock Price")
-plt.plot(optionPriceList, label = "Option Price")
-plt.title("Option Price vs Stock Price")
-plt.legend()
-plt.show()
+def fill_probability(distance, Orders=140, FillRate=1.5 ):
+    OrderIntensity = Orders * np.exp(-distance * FillRate) 
+    Probability = 1 - np.exp(-OrderIntensity * FillRate)
+
+def reservation_price(fair_value, inventory, gamma, sigma, time_remaining):
+    return fair_value - inventory * gamma * sigma**2 * time_remaining
+
+def optimal_spread(gamma, sigma, time_remaining, k):
+    return gamma * sigma**2 * time_remaining + (2 / gamma) * np.log(1 + gamma / k)
